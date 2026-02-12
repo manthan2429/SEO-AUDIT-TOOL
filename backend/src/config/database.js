fc7d0d279e5163868d28702521f11ec5830e75cc
Mkpatel2429 <105143969+Mkpatel2429@users.noreply.github.com>
2026-02-13 01:48:37 +0530

    changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,21 +1,10 @@
 import sqlite3 from "sqlite3";
 import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
-// Needed in ES Modules to get __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Fallback added to prevent crash
-const dbPath = path.resolve(
-	__dirname,
-	process.env.DB_PATH || "./database.sqlite",
-);
-
-console.log("Database Path:", dbPath);
+const dbPath = path.resolve(process.env.DB_PATH);
 
 const db = new sqlite3.Database(dbPath, (err) => {
 	if (err) {
